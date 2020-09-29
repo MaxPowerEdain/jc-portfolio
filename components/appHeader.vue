6bfb3eedb6ad1ca7f3fb20f5bf3d7141a6cc712d
MaxPowerEdain <joancarazo1994@gmail.com>
2020-09-29 12:22:23 +0200

    Update appHeader.vue

--- components/appHeader.vue
+++ components/appHeader.vue
@@ -5,10 +5,10 @@
 
     <b-collapse id="nav-collapse" is-nav>
       <b-navbar-nav class="ml-auto t-nav text-right">
-        <b-nav-item href="/#home">Home</b-nav-item>
-        <b-nav-item href="/#about">About Me</b-nav-item>
-        <b-nav-item href="/#portfolio">Portfolio</b-nav-item>
-        <b-nav-item href="/#contact">Contact</b-nav-item>
+        <b-nav-item to="/#home">Home</b-nav-item>
+        <b-nav-item to="/#about">About Me</b-nav-item>
+        <b-nav-item to="/#portfolio">Portfolio</b-nav-item>
+        <b-nav-item to="/#contact">Contact</b-nav-item>
       </b-navbar-nav>
 
     </b-collapse>
